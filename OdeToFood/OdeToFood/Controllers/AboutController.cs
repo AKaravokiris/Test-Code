208ba9a099cf6ce37af1f79066875cf5c57728e6
AKaravokiris <karavokiris93@hotmail.com>
2018-12-12 13:23:01 +0200

    first commit

---
+++ OdeToFood/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+
+        public string Phone()
+        {
+            return "2106545570";
+        }
+
+
+        public string Address()
+        {
+            return "GREECE";
+        }
+    }
+}
